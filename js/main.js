77ebe66e32559b8cd1d17f97d49b4f3049158f5f
evgeniy <0-0_0@bk.ru>
2022-10-07 00:22:02 +0300

    ternary

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
-function randomNumber (firstNumber, lastNumber) {
-  if ((firstNumber >= 0) && (lastNumber >= 0) && (firstNumber <= lastNumber)) {
-    return Math.round((Math.random() * (lastNumber - firstNumber) + firstNumber));
+function randomNumber(firstNumber, lastNumber) {
+  if (firstNumber >= 0 && lastNumber >= 0 && firstNumber <= lastNumber) {
+    return Math.round(Math.random() * (lastNumber - firstNumber) + firstNumber);
   } else {
     return NaN;
   }
 }
 
-function randomNumberWithComma (firstNumber, lastNumber, symbolsAfterComma) {
-  if ((firstNumber >= 0) && (lastNumber >= 0) && (firstNumber <= lastNumber)) {
-    return ((Math.random() * (lastNumber - firstNumber) + firstNumber).toFixed(symbolsAfterComma));
+function randomNumberWithComma(firstNumber, lastNumber, symbolsAfterComma) {
+  if (firstNumber >= 0 && lastNumber >= 0 && firstNumber <= lastNumber) {
+    return (Math.random() * (lastNumber - firstNumber) + firstNumber).toFixed(symbolsAfterComma);
   } else {
     return NaN;
   }
 }
 
-randomNumber(-0.0, 10.1411);
-randomNumberWithComma(-0.0, 10.1411, 4);
+const randomNumberTernary = (firstNumber, lastNumber) => (firstNumber >= 0 && lastNumber >= 0 && firstNumber <= lastNumber ? Math.round(Math.random() * (lastNumber - firstNumber) + firstNumber) : NaN);
+
+const randomNumberWithCommaTernary = (firstNumber, lastNumber, symbolsAfterComma) => (firstNumber >= 0 && lastNumber >= 0 && firstNumber <= lastNumber ? (Math.random() * (lastNumber - firstNumber) + firstNumber).toFixed(symbolsAfterComma) : NaN);
+
+randomNumber(-0.0, 10);
+randomNumberWithComma(-0.0, 10, 10);
+randomNumberTernary(-0.0, 10);
+randomNumberWithCommaTernary(-0.0, 10, 10);
